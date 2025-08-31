1c14ca9386c2e5a4ab7d3433110b59e8ce6782ac
Amanraj24 <amanrajlahar@gmail.com>
2025-08-31 21:41:03 +0530

    fix: no pass

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { jwtVerify } from 'jose';
 
-const secret = new TextEncoder().encode(process.env.JWT_SECRET || "Aman1234");
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
 export async function middleware(request: NextRequest) {
   try {
